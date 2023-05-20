871dca20ce10895732857a8e1d7b095aa3c55267
VladimirPavlov1 <106485364+VladimirPavlov1@users.noreply.github.com>
2023-05-20 12:53:13 +0000

    Deploying to gh-pages from @ VladimirPavlov1/group_project_6_final@c7e50acc3e654e549cf1cd692884f4fd5231be19 🚀

--- static/js/852.d03f7914.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgroup_project_6_final=self.webpackChunkgroup_project_6_final||[]).push([[852],{852:function(e,a,s){s.r(a);var i=s(184);a.default=function(){return(0,i.jsxs)("div",{className:"mainContainer",children:[(0,i.jsx)("h1",{className:"mainTitle",children:"Take good care of your small pets"}),(0,i.jsxs)("div",{className:"imageContainer",children:[(0,i.jsx)("div",{className:"imageOne"}),(0,i.jsx)("div",{className:"imageTwo"}),(0,i.jsx)("div",{className:"imageThree"})]})]})}}}]);
-//# sourceMappingURL=852.d03f7914.chunk.js.map
