dae29e2905281940a5e5a4d3bd7c158016d8a881
Andrii Bogomolov <bogomolov.andrii@gmail.com>
2023-05-22 20:39:16 +0200

    upd 3

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,21 @@
-// import Logo from '../Logo/Logo';
-// import Nav from '../Nav/Nav';
-// import UserNav from '../UserNav/UserNav';
-// import AuthNav from '../AuthNav/AuthNav';
-// import css from '../Header/Header.module.css';
+import Logo from '../Logo/Logo';
+import Nav from '../Nav/Nav';
+import UserNav from '../UserNav/UserNav';
+import AuthNav from '../AuthNav/AuthNav';
+import css from '../Header/Header.module.css';
 
-// const Header = () => {
-//   return (
-//     <Header className={css.HeaderWrapper}>
-//       <Logo />
-//       <Nav />
-//       <UserNav />
-//       <AuthNav />
-//     </Header>
-//   );
-// };
+const Header = () => {
+  return (
+    <Header className={css.HeaderWrapper}>
+      <Logo />
+      <Nav />
+      <UserNav />
+      <AuthNav />
+    </Header>
+  );
+};
 
-// export default Header;
+export default Header;
 
 // import { Header } from '../Header/Header.module.css';
 
