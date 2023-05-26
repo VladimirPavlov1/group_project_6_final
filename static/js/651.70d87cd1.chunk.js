006c2bbf1faf6f790e06de488321d420e1d280fd
VladimirPavlov1 <106485364+VladimirPavlov1@users.noreply.github.com>
2023-05-26 16:12:56 +0000

    Deploying to gh-pages from @ VladimirPavlov1/group_project_6_final@aec2e97a64e610e4fb53c745f536e2afbaf35299 🚀

---
+++ static/js/651.70d87cd1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgroup_project_6_final=self.webpackChunkgroup_project_6_final||[]).push([[651],{4651:function(e,t,l){l.d(t,{gGS:function(){return r}});var n=l(9983);function r(e){return(0,n.w_)({tag:"svg",attr:{viewBox:"0 0 16 16",fill:"currentColor"},child:[{tag:"path",attr:{fillRule:"evenodd",clipRule:"evenodd",d:"M7 3.093l-5 5V8.8l5 5 .707-.707-4.146-4.147H14v-1H3.56L7.708 3.8 7 3.093z"}}]})(e)}}}]);
+//# sourceMappingURL=651.70d87cd1.chunk.js.map
